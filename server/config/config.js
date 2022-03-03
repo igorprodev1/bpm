77f7e4d779905669fc4f591f49ff7f25cc50b28d
shumik-vladyslav <shumik.vladyslav@gmail.com>
2022-03-03 13:05:10 +0200

    8080

--- server/config/config.js
+++ server/config/config.js
@@ -54,7 +54,7 @@ const config = {
 // MONGO_HOST=mongodb://morovar:aywcz1q8@ds031948.mlab.com:31948/heroku_37kv0jj3
 // MEAN_FRONTEND=angular
 
-  port: 9997,
+  port: 8080,
   mongooseDebug: envVars.MONGOOSE_DEBUG,
   jwtSecret: envVars.JWT_SECRET,
   frontend: envVars.MEAN_FRONTEND || 'angular',
